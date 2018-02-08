2431120eb2611e08b0fc7956888b5d6c0679099a
freedom <freedom.zhangzhijun@gmail.com>
2018-02-08 14:09:04 +0800

    finish lesson2

--- lesson2/test-and-pdf.py
+++ lesson2/test-and-pdf.py
@@ -18,3 +18,12 @@ clf.fit(train_data,train_target)
 
 print test_target
 print clf.predict(test_data)
+
+import graphviz 
+dot_data = tree.export_graphviz(clf, out_file=None, 
+                         feature_names=iris.feature_names,  
+                         class_names=iris.target_names,  
+                         filled=True, rounded=True,  
+                         special_characters=True) 
+graph = graphviz.Source(dot_data) 
+graph.render("iris") 
